f91aea72c611b0a3507823def9a0988655ae2716
ZX-10R <vallierekuma@gmail.com>
2021-11-10 11:08:10 +0900

    nothing specialy to write

--- works/stopwatch/stopwatch.js
+++ works/stopwatch/stopwatch.js
@@ -2,9 +2,10 @@ window.onload = loadFunction();
 
 function loadFunction(){
 
-
+    var time_now = Date.now();
     var time = new Date();
     var now = time.toTimeString ();
     console.log(now);
-
+    console.log(time);
+    console.log(time_now);
 }
