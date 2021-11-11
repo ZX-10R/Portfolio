5e712f15e9e5009c8a57633395c5418b506e4729
kuma <vallierekuma@gmail.com>
2021-11-12 00:10:46 +0900

    add StopwatchFace to test.js

--- works/test/test.js
+++ works/test/test.js
@@ -1,9 +1,11 @@
-window.onload = loadFunction();
+'use strict';
+window.onload = fStopwatch();
 
-function loadFunction(){
-
-    
+function fStopwatch(){
+    'use strict';
     var time = Date.now();
     console.log(time);
     document.write(time);
+    var stopwatchDiv = document.createElement("div");
+    var stopwatchFace = document.
 }
