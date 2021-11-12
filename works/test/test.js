aa3feaac613c2479922bcb4ae4d7b71a7b715765
ZX-10R <vallierekuma@gmail.com>
2021-11-12 14:55:13 +0900

    start onclick func under construction *(alert not working on)

--- works/test/test.js
+++ works/test/test.js
@@ -1,11 +1,29 @@
 'use strict';
-window.onload = fStopwatch();
+window.onload = function(){
 
-function fStopwatch(){
-    'use strict';
-    var time = Date.now();
-    console.log(time);
-    document.write(time);
-    var stopwatchDiv = document.createElement("div");
-    var stopwatchFace = document.
-}
+    let milisec = 0;
+    let sec = 0;
+    let min = 0;
+    let hour = 0;
+    var timeNow = Date.now();
+    console.log(timeNow);
+    var timer_face = document.getElementById("timer_face");
+    var start = document.getElementsByName("btn_start");
+    var stop = document.getElementsByName("btn_stop");
+    var reset = document.getElementsByName("btn_reset");
+    console.log(timer_face);
+    console.log(start);
+    console.log(stop);
+    console.log(reset);
+
+    // start押されたとき
+    start.onclick = function(){
+        alert("!!");
+    };
+    
+    
+    
+
+};
+
+// setInterval(関数function, 一定時間の指定)
