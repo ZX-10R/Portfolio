6862b5d74c766499100b26bd76142e7baf732949
ZX-10R <vallierekuma@gmail.com>
2021-11-15 15:16:18 +0900

    add new Functions not worked with NAN

--- works/test/test.js
+++ works/test/test.js
@@ -1,29 +1,42 @@
 'use strict';
 window.onload = function(){
-
+    
     let milisec = 0;
     let sec = 0;
     let min = 0;
     let hour = 0;
-    var timeNow = Date.now();
-    console.log(timeNow);
     var timer_face = document.getElementById("timer_face");
-    var start = document.getElementsByName("btn_start");
-    var stop = document.getElementsByName("btn_stop");
-    var reset = document.getElementsByName("btn_reset");
+    var start = document.getElementById("btn_start");
+    var stop = document.getElementById("btn_stop");
+    var reset = document.getElementById("btn_reset");
+    function displayFunction(time){
+        Number(time);
+        console.log(typeof(time));
+        var timeDelta = Date.now() - time;
+        timer_face.textContent = timeDelta;
+    }
+    // start押されたとき
+    start.onclick = function(){
+        var timeNow = Date.now();
+        console.log(typeof(timeNow));
+        setInterval(function(){displayFunction(timeNow)}, 1000); 
+    }
+    // stop
+    stop.onclick= function(){
+        var timeStop = Date.now();
+        console.log(timeStop);
+    }
+    // reset
+    reset.onclick = function(){
+        var timeDelta = 0;
+    }
     console.log(timer_face);
     console.log(start);
     console.log(stop);
     console.log(reset);
+    setInterval(displayFunction);
+} 
 
-    // start押されたとき
-    start.onclick = function(){
-        alert("!!");
-    };
-    
-    
-    
-
-};
 
 // setInterval(関数function, 一定時間の指定)
+// Date.now =1000x 60 x 60 x
